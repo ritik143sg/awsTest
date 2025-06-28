cda5e6baecb578e5a4df24b1d6811d2c818731c1
ritik143sg <ritik143sg@gmail.com>
2025-06-28 17:54:53 +0530

    3rd commit

--- userTest/backend/server.js
+++ userTest/backend/server.js
@@ -8,6 +8,8 @@ const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "frontend")));
+
 app.use(cors());
 
 dotenv.config();
